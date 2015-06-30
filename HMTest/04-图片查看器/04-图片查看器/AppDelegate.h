1f6b81136a4031fc8f23c68c0ebf3dc89df6499b
gobytony <gobyhl@126.com>
2015-06-30 22:10:39 +0800

    Initial Commit

---
+++ HMTest/04-图片查看器/04-图片查看器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  04-图片查看器
+//
+//  Created by Jack on 15/6/30.
+//  Copyright (c) 2015年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
